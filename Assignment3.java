c0f0eee611079319ea156fef640352083dd5c8b1
mongi008 <orpillagian123@gmail.com>
2026-09-01 21:18:30 +0800

    gian pogi

---
+++ Assignment3.java
@@ -0,0 +1,21 @@
+public class Assignment3 {
+    public static void main(String[] args) {
+        // just hardcoding the info directly, no need to type it in every time
+        String firstname = "Gian Carlo";
+        String lastname = "Orpilla";
+        String address = "Urdaneta City";
+        String mobile = "09639937214"; // string so we don't lose the leading 0
+        int age = 18;
+        double gwa = 95.24;
+
+        // now just print it all out
+
+
+        System.out.println("Firstname: " + firstname  );
+        System.out.println("Lastname:: " + lastname  );
+        System.out.println("Address: " + address);
+        System.out.println("Mobile Number: " + mobile);
+        System.out.println("Age: " + age);
+        System.out.printf("GWA: "+ gwa);
+    }
+}
