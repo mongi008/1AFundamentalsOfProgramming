8f308c771bd0918ee534c80ccfa1e7789af122e7
mongi008 <orpillagian123@gmail.com>
2026-09-23 20:30:14 +0800

    Update SecondJava.java

--- SecondJava.java
+++ SecondJava.java
@@ -4,12 +4,12 @@ public class SecondJava {
         String add = "urdaneta";
         int age = 18;
         String contact = "09639937214";
-        String crush = "NIGGA";
+        String crush = "sir leo";
 
         System.out.println("NAME: " + name);
         System.out.println("lugar: " + add);
         System.out.println("edad mo kofal: " + age);
         System.out.println("tawagan ang pogi: " + contact);
-        System.out.println("ayieeeee: " + crush);
+        System.out.println("Crush: " + crush);
     }
 }
